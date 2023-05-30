deb21295e82bfa9f677363d8ce98daa936737d1e
Ron Braha <ron.braha@icloud.com>
2023-05-30 16:31:58 -0400

    build: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-source venv/bin/activate
-sudo git pull origin main
-sudo pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+sudo pip3 install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
--- worker_deploy.sh
+++ worker_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-source venv/bin/activate
-sudo git pull origin main
-sudo pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
+sudo pip3 install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
 sudo /etc/init.d/celeryd restart
