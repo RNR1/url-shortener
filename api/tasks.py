dc2f21b99f78a9b07817645692ee9e58b51df581
Ron Braha <ron.braha@icloud.com>
2023-06-03 16:13:02 -0400

    fix: task log

--- api/tasks.py
+++ api/tasks.py
@@ -1,12 +1,14 @@
-from celery import shared_task
+from celery import shared_task, Task
 
 from api.serializers import VisitorSerializer
 
 
 @shared_task
 def collect_visitor_data(ip_address, hash):
+    print(ip_address, hash)
     serializer = VisitorSerializer(
         data={'ip_address': ip_address, 'url': hash})
-    serializer.is_valid(raise_exception=True)
-
-    serializer.create(serializer.validated_data)
+    if serializer.is_valid():
+        serializer.create(serializer.validated_data)
+    else:
+        print(serializer.errors)
